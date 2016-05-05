de73935576fdac3e68bc6872b9c730988e109079
audric-perrin <audric.perrin@gmail.com>
2016-05-05 15:30:58 +0200

    erreur modification stats

--- app/scripts/react/loggedInApp/statsManager.js
+++ app/scripts/react/loggedInApp/statsManager.js
@@ -8,19 +8,37 @@ var d = React.DOM
 //Composant manager records
 var StatsManager = React.createClass({
   displayName: 'statsManager',
+  getInitialState: function() {
+    return {isLoading: true, records: []}
+  },
+  isLoading: function() {
+    return d.div({
+      style: {
+        fontSize: '16px',
+        marginTop: '10px',
+        padding: '15px 0',
+        color: COLOR.black,
+        backgroundColor: COLOR.white
+      }
+    }, d.i({
+      style: {
+        display: 'block',
+        fontSize: '40px',
+        marginBottom: '5px',
+        color: COLOR.gray3
+      },
+      className: "fa fa-spinner fa-pulse"
+    }), "Chargement des Classements")
+  },
   render: function() {
     if (this.state.isLoading) {
-      elements = this.isLoading()
+      var elements = this.isLoading()
     }
-    else {
-      var elements = []
-      for (var i = 0; i < this.state.records.length; i++) {      
-        elements.push(React.createElement(BoxRecords, {
-          key: i,
-          data: this.state.records[i]
-        }))
-      }
-    }
+    // else {
+    //   var elements = []
+    //   for (var i = 0; i < this.state.records.length; i++) {      
+    //     elements.push(React.createElement(BoxRecords, {
+    // }
     return d.div({
       style: {}
     }, elements)

df119327bd787c6d13d23a6616b361e9924a9d47
audric-perrin <audric.perrin@gmail.com>
2016-08-06 12:15:24 +0200

    modification userInfo pour deploy

--- lib/usersInfo.php
+++ lib/usersInfo.php
@@ -1,6 +1,7 @@
 <?php
   require_once('../php/sql.php');
   require_once('../lib/general.php');
+
   function getUsersInfo() {
     $result = runQuery('SELECT * FROM user ORDER BY pseudo');
     foreach ($result as $row) {
@@ -11,6 +12,7 @@
     }
     return $users;
   }
+  
   function getUserInfo() {
     if (isset($_SESSION['id'])) {
       $result = runQuery('SELECT * FROM user WHERE id = ' . $_SESSION['id']);
